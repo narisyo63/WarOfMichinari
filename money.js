14fc4d1b8c0a1ab6e9f8c0f4b473749a282f3ca0
michimichix521 <michimichix521@gmail.com>
2019-08-15 20:30:38 +0900

    Update money.js

--- money.js
+++ money.js
@@ -3,5 +3,9 @@ class Money{
 		this.value=value;
 	}
 
-	
+	drawMoneyValue(){
+		this.canvas.fillStyle="black";
+		this.canvas.font="48px serif";
+		this.canvas.fillText(this.value+"円",400,50);
+	}
 }
