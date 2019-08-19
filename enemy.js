de98cff5622c10bfb43c2cdebadbe0e1bf71e961
narisyo63 <43071694+narisyo63@users.noreply.github.com>
2019-08-19 21:43:57 +0900

    Update enemy.js

--- enemy.js
+++ enemy.js
@@ -23,26 +23,26 @@ class Enemy
 	{
 		this.image.src=image;
 	}
-	showImage(px, py)
+	showImage()
 	{
 		this.canvas.drawImage(this.image,(this.n%2)*32,(this.num-1)*32,32,32,this.x-25,this.y-25,50,50);		
 		this.n++;
 	}
 	hitJudge(player)	//敵との当たり判定
 	{
-		let distance=Math.sqrt((player.x-this.x)**2+(player.y-this.y)**2);
-		if(distance<=50){
-			this.hp-=player.damage;
+		for(let i = 0; i < player.length; i++){
+			let distance=Math.sqrt((player[i].x-this.x)**2+(player[i].y-this.y)**2);
+			if(distance<=50){
+				this.hp-=player.damage;
+				return true;
+			}else{
+				return false;
+			}
 		}
 	}
-	move(player)
+	move()
 	{
-		let distance=Math.sqrt((player.x-this.x)**2+(player.y-this.y)**2);
-		if(distance<=50){
-			this.moveEnemy = false;
-		}else{
-			this.x+=1*this.speed;
-		}
+		this.x+=1*this.speed;
 	}
 }
 class Enemy1 extends Enemy{
@@ -52,7 +52,7 @@ class Enemy1 extends Enemy{
 		this.num = 1;
 		this.cost = 50;
 		this.damage=10;
-		this.speed = 1;
+		this.speed = 1.5;
 		this.HP = 100;
 
 	}
@@ -73,7 +73,7 @@ class Enemy3 extends Enemy{
 		super(canvas,image,x,y);
 
 		this.num = 3
-		this.cost = 300;
+		this.cost = 200;
 		this.damage=50;
 		this.speed = 0.5;
 		this.HP = 200;
@@ -101,8 +101,8 @@ class EnemyGenerator
 	generator(player)
 	{
 		this.rd = Math.floor( Math.random() * 50);
-		console.log(this.eneCnt[1]);
-		
+		console.log(this.exp);
+
 		this.exp++;
 		if(this.exp >= 50){
 			if(this.eneCnt[1] < 2){
@@ -112,28 +112,27 @@ class EnemyGenerator
 				this.exp -= 50;
 			}
 			if(this.exp >= 100){
-				if(this.eneCnt[2] < 2)
+				if(this.eneCnt[2] < 2){
 				this.enemy_list.push(new Enemy2(this.canvas,this.image,this.x,this.y));
 				this.alleneCnt++;
 				this.eneCnt[2]++;
 				this.exp -= 100;
-
-				if(this.exp >= 300){
+				}
+				if(this.exp >= 200){
 					this.enemy_list.push(new Enemy3(this.canvas,this.image,this.x,this.y));
 					this.alleneCnt++;
 					this.eneCnt[3]++;
-					this.exp -= 300;
-					}
+					this.exp -= 200;
+				}
 			}
 		}
 
 
 		for(let i = 0; i < this.enemy_list.length; i++){
 			if(this.enemy_list[i] != null){
-				for(let j = 0; j < player.length; j++){
-					this.enemy_list[i].hitJudge(player[j]);
-					this.enemy_list[i].showImage(player[j].x,player[j].y);
-					this.enemy_list[i].move(player[j]);
+				this.enemy_list[i].showImage();
+				if(!this.enemy_list[i].hitJudge(player)){
+					this.enemy_list[i].move();
 				}
 			}
 			if(this.enemy_list[i].hp <= 0 && this.enemy_list[i] != null){
